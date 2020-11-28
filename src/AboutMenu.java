e2f7a3f6a1ca1274b26a63cd29a721dba6d1756f
mushroom0956 <72372063+mushroom0956@users.noreply.github.com>
2020-11-27 20:15:21 -0700

    Added class description in javadoc format

--- src/AboutMenu.java
+++ src/AboutMenu.java
@@ -1,3 +1,25 @@
+/**
+ * Name			Rebecca Kraft
+ *				Justin De Ocampo
+ *				Kevin Weinhold
+ *				Ishrar Zaman
+ * 				Lingge Zhang
+ *
+ * Email 		rmkraft@asu.edu
+ * 				jcdeocam@asu.edu
+ * 				kweinho2@asu.edu		
+ *				izaman1@asu.edu
+ * 				lzhan264@asu.edu
+ * 
+ * Class		CSE 360	70605
+ * Final Project
+ * 
+ * This class displays group members' names in a dialog box. 
+ * 
+ * @author 		
+ * 
+ */
+
 import javax.swing.*;
 
 public class AboutMenu {
