ce775896c59c09b090902c959d42a7a2c0c8d482
mushroom0956 <72372063+mushroom0956@users.noreply.github.com>
2020-11-30 00:40:07 -0700

    Added dialog box

--- src/AttendanceLoader.java
+++ src/AttendanceLoader.java
@@ -21,7 +21,6 @@
  * @author
  *
  */
-
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
@@ -43,18 +42,20 @@ import javax.swing.JFormattedTextField.AbstractFormatter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class AttendanceLoader extends Menu_Bar {
 
 
-    File selectedFile = null;
-    Vector<String> times = new Vector<>();
-    static ArrayList<String[]> attendanceInfo = new ArrayList<>();
+    protected File selectedFile = null;
+    protected Vector<String> times = new Vector<>();
+    protected static ArrayList<String[]> attendanceInfo = new ArrayList<>();
 
-    public void chooseAttendanceFile() {
-
-        try {
-            //chooses the attendance CSV and reads from it
+    /**
+     * This method asks user for a file path. Then add the attendance
+     * at the end of the table
+     */
+    public void chooseAttendanceFile(){
+        try{
+            // clear the previous data
             times.clear();
             attendanceInfo.clear();
             //chooses the attendance CSV and reads from it
@@ -87,6 +88,9 @@ public class AttendanceLoader extends Menu_Bar {
                     }
                 }};
 
+                //create a map for extra attendee
+                Map<String, Integer> extraMap = new HashMap<String, Integer>();
+
                 // iterate through attendanceInfo
                 for(int j = 0; j < attendanceInfo.size(); j++){
                     // if the hash set of ASURITE ID's matches the attendanceInfo
@@ -94,7 +98,22 @@ public class AttendanceLoader extends Menu_Bar {
                         // if the person has multiple inputs, add together their minutes
                         //map.put(key, map.get(key) + value);
                         //map.put(key, (previous value) + current value);
-                        attendanceMap.put(attendanceInfo.get(j)[0], attendanceMap.get(attendanceInfo.get(j)[0]) + Integer.parseInt(attendanceInfo.get(j)[1]));
+                        attendanceMap.put(attendanceInfo.get(j)[0], attendanceMap.get(attendanceInfo.get(j)[0])
+                                + Integer.parseInt(attendanceInfo.get(j)[1]));
+
+                    }
+
+                    //add extra attendee to a new map
+                    else{
+                        extraMap.put(attendanceInfo.get(j)[0],tNum);
+                    }
+                }
+
+                //if extra attendees have multiple inputs, add together their minutes
+                for(int t = 0; t < attendanceInfo.size(); t++){
+                    if(!attendanceMap.containsKey(attendanceInfo.get(t)[0])) {
+                        extraMap.put(attendanceInfo.get(t)[0], extraMap.get(attendanceInfo.get(t)[0])
+                                + Integer.parseInt(attendanceInfo.get(t)[1]));
                     }
                 }
 
@@ -105,8 +124,11 @@ public class AttendanceLoader extends Menu_Bar {
                     times.add(Integer.toString(attendanceMap.get(row[5])));
                 }
                 // once we obtain full times vector, we pick the date and GO
-                pickDate(times);
+                pickDate(times, extraMap);
                 //data.updateColumns(date, times);
+
+
+
             }
         }
         //Catching errors
@@ -118,13 +140,19 @@ public class AttendanceLoader extends Menu_Bar {
         }
     }
 
-    public void pickDate(Vector<String> times){
-
-        createWindow(times);
-
+    /**
+     * This function calls the helper function createWindow.
+     *
+     * @param times a vector that holds attendance info of the users that are in the roster
+     * @param map a map that holds the attendance info of the extra attendee
+     */
+    public void pickDate(Vector<String> times, Map<String, Integer> map){
+        createWindow(times, map);
     }
 
-    //need this class for date picker
+    /**
+     * This class is needed for date picker
+     */
     public static class DateLabelFormatter extends AbstractFormatter {
         private String datePattern = "MM-dd-yyyy";
         private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
@@ -143,11 +171,16 @@ public class AttendanceLoader extends Menu_Bar {
 
             return "";
         }
-
     }
 
-
-    private static String createWindow(Vector<String> times) {
+    /**
+     * This is a helper function creates a new JFrame that allows users to
+     * pick a date in a new window.
+     *
+     * @param times a vector that holds attendance info of the users that are in the roster
+     * @param map a map that holds the attendance info of the extra attendee
+     */
+    private static void createWindow(Vector<String> times, Map<String, Integer> map) {
         final String[] input = {""};
         JFrame frame = new JFrame("Pick a Date using ... button");
         UtilDateModel model = new UtilDateModel();
@@ -165,21 +198,49 @@ public class AttendanceLoader extends Menu_Bar {
                 // get date as a string
 
                 input[0] = datePicker.getJFormattedTextField().getText();
-
-                System.out.println("Input value: " + input[0]);
-                System.out.println("\nTimes value: " + times.toString());
+                
                 data.updateColumns(input[0], times);
                 // Update the original file with new Columns of the date we chose
                 //data.updateColumns(input[0]);
+                dialogBox(times, map);
 
             }
         });
 
-        frame.setSize(560, 200);
+        frame.setSize(360, 200);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-        return input[0];
     }
 
+    /**
+     * This method creates a dialog box displaying how many data are loaded for users
+     * in the roster and how many additional attendee were found
+     * @param times the vector that contains users in the roster
+     * @param map the map that contains extra attendees
+     */
+    private static void dialogBox(Vector<String> times, Map<String, Integer> map){
+
+        //how many users' data are loaded
+        int size = times.size();
+        String str = "Data loaded for "+size+" users in the roster.\n\n";
+
+        //how many extra attendees were founded
+        int mapSize = map.size();
+        String str1 = mapSize + " additional attendee was found:\n\n";
+
+        //extra attendees' info
+        StringBuffer s = new StringBuffer(str);
+        s.append(str1);
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            s.append(entry.getKey());
+            s.append(", connected for ");
+            s.append(entry.getValue());
+            s.append(" minutes\n");
+        }
+
+        //display all the info on the dialog box
+        JOptionPane.showMessageDialog(null,s,
+                "Display Message",JOptionPane.INFORMATION_MESSAGE);
+    }
 }
+
